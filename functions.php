c66906b8b73b0142ba33017d3dbc0cb706473126
Ana CN <an.ceron@duocuc.cl>
2026-04-17 14:09:11 -0400

    agregando main css a funtions

--- functions.php
+++ functions.php
@@ -3,4 +3,12 @@
 require_once get_template_directory() . '/includes/enqueue.php';
 require_once get_template_directory() . '/includes/panel-empresa-ajax.php';
 
-
+function mass_enqueue_styles() {
+    wp_enqueue_style(
+        'mass-main',
+        get_template_directory_uri() . '/assets/css/main.css',
+        array(),
+        '1.0'
+    );
+}
+add_action('wp_enqueue_scripts', 'mass_enqueue_styles');
