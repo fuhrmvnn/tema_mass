943be5c4b99c7d4e8ca1fae20f11fc563d238f49
coni-cp <c.parraguezs@duocuc.cl>
2026-04-17 18:17:01 -0400

    vamos hooks

--- single-courses.php
+++ single-courses.php
@@ -31,50 +31,12 @@ while (have_posts()) : the_post();
         <aside class="curso-sidebar">
 
             <h3>Contenido del curso</h3>
-                <?php
-                    $course_id = get_the_ID();
-                    $topics = tutor_utils()->get_topics($course_id);
 
-                    if ($topics) :
-                        foreach ($topics as $topic) :
-                    ?>
+            <?php do_action('tutor_course/single/lessons'); ?>
 
-                    <div class="accordion-item">
-
-                        <div class="accordion-header">
-                            <?php echo esc_html($topic->post_title); ?>
-                            <span class="arrow">⌄</span>
-                        </div>
-
-                        <div class="accordion-content">
-
-                            <?php
-                            $contents = tutor_utils()->get_topic_contents($topic->term_id);
-
-                            if ($contents) :
-                                foreach ($contents as $item) :
-                            ?>
-
-                                <div class="leccion-item">
-                                    <?php echo esc_html($item->post_title); ?>
-                                </div>
-
-                            <?php endforeach; endif; ?>
-
-                            <!-- botón quiz (simulado o dinámico) -->
-                            <a href="#" class="btn-quiz">Ir al Quiz</a>
-
-                        </div>
-
-                    </div>
-
-                    <?php
-                        endforeach;
-                    endif;
-                ?>
-           
         </aside>
 
+        <?php do_action('tutor_course/single/after/main_content'); ?>
           
 
     </div>
